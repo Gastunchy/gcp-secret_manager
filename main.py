6b9497953707918b607af97300833be475ee5948
gastunchy <fariasgastondavid@gmail.com>
2025-03-03 15:56:54 -0300

    buscador de variable

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from flask import Flask, jsonify
-from google.cloud import secretmanager
+from flask import Flask, render_template_string
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def access_secret():
-    try:
-        client = secretmanager.SecretManagerServiceClient()
-        secret_name = "projects/488709866434/secrets/test-base-secret/versions/lates"
-        secret = client.access_secret_version(request={"name": secret_name}).payload.data.decode("UTF-8")
-        return jsonify({"secret": secret})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+def mostrar_contenido():
+    dia = os.getenv('DIA')
+    mes = os.getenv('MES')
+    año = os.getenv('AÑO')
+    
+    contenido = f"""
+    <h1>Contenido de las Variables de Entorno</h1>
+    <p>Día: {dia}</p>
+    <p>Mes: {mes}</p>
+    <p>Año: {año}</p>
+    """
+    return render_template_string(contenido)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+if __name__ == '__main__':
+    app.run(debug=True)
